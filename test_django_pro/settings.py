f47b0190c9fea00de1fbab846888553d06ff9a41
ridhindas <ridhindas@gmail.com>
2026-07-07 11:07:02 +0530

    Update settings.py

--- test_django_pro/settings.py
+++ test_django_pro/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5cqh@^@7g&-03mk-x-o=xht0^jfu=ujx#i4kr2q#*p@eq71=sy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.66.158.97', 'localhost', '127.0.0.1']
 
 
 # Application definition
